1e98c1314514bed6a5ed4d207cfe802e8b5375fc
Tad Thorley <phaedryx@gmail.com>
2011-10-30 20:01:38 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloSpine::Application.config.secret_token = '39d3f9c2a723cf85db8b0b438edeec96e3657195153398d1a8bd85e2d98c600cfa7bf49a99f0b4ddb81adb3a00002dc4f3a18645100c45e46a457e60f9478e76'
